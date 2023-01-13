f6f0f037f20ab4ed89f68b33e0c29e5ba87a8f90
Patricia Angélica Martínez-Polo Johnson <72499569+alu0101116532@users.noreply.github.com>
2023-01-13 19:39:14 +0000

    actualizando

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,27 @@
+import Link from 'next/link'
 import Head from "next/head";
 import styles from "./index.module.css";
-import Link from 'next/link'
 
 function Home() {
+
   return (
-    <ul>
-      <li>
-        <Link href="/post/abc">Go to pages/post/[pid].js</Link>
-      </li>
-      <li>
-        <Link href="/post/abc?foo=bar">Also goes to pages/post/[pid].js</Link>
-      </li>
-      <li>
-        <Link href="/post/abc/a-comment">
-          Go to pages/post/[pid]/[comment].js
-        </Link>
-      </li>
-    </ul>
+    <div>
+      <Head>
+        <title>Inicio</title>
+        <link rel="icon" href="/photo_icon.png" />
+      </Head>
+
+      <main className={styles.main}>
+        <img src="/photo_icon.png" className={styles.icon} />
+        <h2>Inicio</h2>
+        <h3>Eliga un enlace</h3>
+        <Link href="/pet"> Generador de nombres para tu mascota </Link>
+        <br></br>
+        <Link href="/image">Generador de imágenes</Link>
+        <br></br>
+        <Link href="/404">Página de error</Link>
+      </main>
+    </div>
   )
 }
 
