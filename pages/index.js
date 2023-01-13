0b5e4e205b455eba13348fd4a6742790b56fd945
Patricia Angélica Martínez-Polo Johnson <72499569+alu0101116532@users.noreply.github.com>
2023-01-13 20:11:53 +0000

    fin

--- pages/index.js
+++ pages/index.js
@@ -7,16 +7,16 @@ function Home() {
   return (
     <div>
       <Head>
-        <title>Hola!!</title>
+        <title>Hola! Soy un generador</title>
         <link rel="icon" href="/api.png" />
       </Head>
 
       <main className={styles.main}>
-        <img src="/dog.png" alt="" width="100%" height="100%" className={styles.icon} />
+        <img src="/dog.png" alt="" width="200%" height="200%" className={styles.icon} />
         <Link href="/pet">Genera un nombre de mascota</Link>
         <br></br>
-        <img src="/imagen.png" className={styles.icon} />
-        <Link href="/image">Genera una imágen</Link>
+        <img src="/imagen.png" alt="" width="200%" height="200%" className={styles.icon} />
+        <Link href="/image">Genera una imagen</Link>
         <br></br>
         <Link href="/404">Error 404</Link>
       </main>
